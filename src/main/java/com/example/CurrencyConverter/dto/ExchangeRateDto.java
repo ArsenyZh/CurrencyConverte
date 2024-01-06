77523fad74523180c58b1917cc22659fec8508e9
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-01-06 23:40:13 +0300

    refactor/ change fields in dto

--- src/main/java/com/example/CurrencyConverter/dto/ExchangeRateDto.java
+++ src/main/java/com/example/CurrencyConverter/dto/ExchangeRateDto.java
@@ -1,14 +1,13 @@
 package com.example.CurrencyConverter.dto;
 
+import com.example.CurrencyConverter.entity.Currency;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ExchangeRateDto {
-    private Long baseCurrencyId;
-    private Long targetCurrencyId;
-    private String baseCurrency;
-    private String targetCurrency;
+    private Currency baseCurrency;
+    private Currency targetCurrency;
     private double rate;
 }
