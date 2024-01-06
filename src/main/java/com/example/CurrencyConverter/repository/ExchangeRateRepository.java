18f319c92e239e542e29366719214a2da62ff6b2
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-01-06 21:41:39 +0300

    fix/

--- src/main/java/com/example/CurrencyConverter/repository/ExchangeRateRepository.java
+++ src/main/java/com/example/CurrencyConverter/repository/ExchangeRateRepository.java
@@ -1,10 +1,11 @@
 package com.example.CurrencyConverter.repository;
 
+import com.example.CurrencyConverter.entity.Currency;
 import com.example.CurrencyConverter.entity.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
-    ExchangeRate findByBaseCurrencyAndTargetCurrency(Long baseCurrency, Long targetCurrency);
+    ExchangeRate findByBaseCurrencyAndTargetCurrency(Currency baseCurrency, Currency targetCurrency);
 }
