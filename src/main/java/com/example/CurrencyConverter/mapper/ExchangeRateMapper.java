73629d6448d63e54eb257c8099d9e561213badd4
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-01-06 20:11:26 +0300

    add/ new mapper for exchangeRate

---
+++ src/main/java/com/example/CurrencyConverter/mapper/ExchangeRateMapper.java
@@ -0,0 +1,60 @@
+package com.example.CurrencyConverter.mapper;
+
+import com.example.CurrencyConverter.dto.ExchangeRateDto;
+import com.example.CurrencyConverter.entity.ExchangeRate;
+import com.example.CurrencyConverter.repository.ExchangeRateRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class ExchangeRateMapper {
+    private ExchangeRateRepository exchangeRateRepository;
+
+    public ExchangeRate toExchangeRate(ExchangeRateDto exchangeRateDto) {
+        return exchangeRateRepository.findByBaseCurrencyAndTargetCurrency(exchangeRateDto.getBaseCurrencyId(),
+                                                                            exchangeRateDto.getTargetCurrencyId());
+    }
+
+    public List<ExchangeRate> toExchangeRateList(List<ExchangeRateDto> exchangeRateDtoList) {
+        List<ExchangeRate> exchangeRateList = new ArrayList<>();
+
+        for (ExchangeRateDto dto : exchangeRateDtoList) {
+            exchangeRateList.add(exchangeRateRepository.findByBaseCurrencyAndTargetCurrency(dto.getBaseCurrencyId(),
+                                                                                            dto.getTargetCurrencyId()));
+        }
+        return exchangeRateList;
+    }
+
+    public ExchangeRateDto toExchangeRateDto(ExchangeRate exchangeRate) {
+        ExchangeRateDto exchangeRateDto = new ExchangeRateDto();
+
+        exchangeRateDto.setBaseCurrency(exchangeRate.getBaseCurrency().getCode());
+        exchangeRateDto.setTargetCurrency(exchangeRate.getTargetCurrency().getCode());
+        exchangeRateDto.setBaseCurrencyId(exchangeRate.getBaseCurrency().getId());
+        exchangeRateDto.setTargetCurrencyId(exchangeRate.getTargetCurrency().getId());
+        exchangeRateDto.setRate(exchangeRate.getRate());
+
+        return exchangeRateDto;
+    }
+
+    public List<ExchangeRateDto> exchangeRateDtoList(List<ExchangeRate> exchangeRateList) {
+        List<ExchangeRateDto> exchangeRateDtoList = new ArrayList<>();
+        ExchangeRateDto exchangeRateDto = new ExchangeRateDto();
+
+        for (ExchangeRate exchangeRate : exchangeRateList) {
+            exchangeRateDto.setBaseCurrency(exchangeRate.getBaseCurrency().getCode());
+            exchangeRateDto.setTargetCurrency(exchangeRate.getTargetCurrency().getCode());
+            exchangeRateDto.setBaseCurrencyId(exchangeRate.getBaseCurrency().getId());
+            exchangeRateDto.setTargetCurrencyId(exchangeRate.getTargetCurrency().getId());
+            exchangeRateDto.setRate(exchangeRate.getRate());
+
+            exchangeRateDtoList.add(exchangeRateDto);
+        }
+        return exchangeRateDtoList;
+    }
+
+}
